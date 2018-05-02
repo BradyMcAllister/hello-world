2e80c6b6ff62b5acd86dba79bc576a57c39219e7
Brady McAllister <brady.mcallister@pcc.edu>
2018-05-01 21:06:07 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by brady.mcallister on 5/1/2018.
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
